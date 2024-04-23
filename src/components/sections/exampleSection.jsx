94284106abd21e7136702df7bb4efed13d27a25e
arminsafie <arminsafaie92@gmail.com>
2024-04-23 07:33:48 +0330

    final 2

--- src/components/sections/exampleSection.jsx
+++ src/components/sections/exampleSection.jsx
@@ -11,7 +11,7 @@ export default function ExampleSection() {
   return (
     <Section title={"Examples"} id="examples">
       <Tabs
-        buttonsContainer="menu"
+        // buttonsContainer="menus"
         buttons={
           <>
             <TabButton
